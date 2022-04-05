b9bfbe8782d6d4fbf60c9140dd1f719a56cb12ea
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-05 16:29:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
-import {} from './cooking-times'
+import cookingTimes {   frenchBeans,
+   sprouts,
 
-import {} from './cup1-2'
-import {} from './cup1-4'
-import {} from './cup1-8'
-import {} from './cup1'
-import {} from './cup3_4'
-import {} from './dash'
-import {} from './gallon1'
-import {} from './pinch'
-import {} from './pint1'
-import {} from './quart1'
-import {} from './tablespoon-1'
-import {} from './teaspoon-1'
+   jacketPotato,
+   sweetPotato,
+
+   kale,
+   leek,
+   broccoli,
+   carrots,
+   mushrooms,
+   onions_shallots,
+   parsnips,
+   peppers,
+   marrow_squash,
+   sweetcorn,
+   courgettes
+ } from './cooking-times'
+
+import { cup1_2 } from './cup1-2'
+import { cup1_4 } from './cup1-4'
+import { cup1_8 } from './cup1-8'
+import { cup1 } from './cup1'
+import { cup3_4 } from './cup3_4'
+import { dash } from './dash'
+import { gallon } from './gallon1'
+import { pinch } from './pinch'
+import { pint1} from './pint1'
+import { quart1} from './quart1'
+import { tablespoon1 } from './tablespoon'
+import { teaspoon } from './teaspoon'
 
 
 export default {
-
+  cookingTimes,
+  cup1_2,
+  cup1_4,
+  cup1_8,
+  cup1,
+  cup3_4,
+  dash,
+  gallon,
+  pinch,
+  pint1,
+  quart1,
+  tablespoon1,
+  teaspoon
 }
