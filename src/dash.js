06b5bb9d0db058ca86b403bc6833bac7023cde75
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-05 05:54:06 +0300

    Update dash.js

--- src/dash.js
+++ src/dash.js
@@ -1,8 +1,10 @@
 
 
-   let dash = {
-         americanStandart: "1/8 teaspoon",
-         americanStandartOunces: null,
-         volume: null,
-         weight: null
-       };
+let dash = {
+   americanStandart: "1/8 teaspoon",
+   americanStandartOunces: null,
+   volume: null,
+   weight: null
+ };
+
+export default dash;
