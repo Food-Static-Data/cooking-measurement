2a2ba9e84afb14ebebc6416a3469a9db26959c14
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-05 05:53:03 +0300

    Update cup1-8.js

--- src/cup1-8.js
+++ src/cup1-8.js
@@ -1,8 +1,9 @@
+//  1/8 cup
+let cup1_8 = {                              
+      americanStandart: "2 tablespoons",
+      americanStandartOunces: "1 ounce",
+      volume: "30 milliliters",
+      weight: "28.3 grams"
+ };
 
-
-   let cup1_8 = {                                //  1/8 cup
-         americanStandart: "2 tablespoons",
-         americanStandartOunces: "1 ounce",
-         volume: "30 milliliters",
-         weight: "28.3 grams"
-       };
+export default cup1_8;
