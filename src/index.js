94c59a54fc91c7ca86f3ebca7c14c0859bebca6d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-05 05:26:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-xxx
+import {} from './cooking-times'
+
+import {} from './cup1-2'
+import {} from './cup1-4'
+import {} from './cup1-8'
+import {} from './cup1'
+import {} from './cup3_4'
+import {} from './dash'
+import {} from './gallon1'
+import {} from './pinch'
+import {} from './pint1'
+import {} from './quart1'
+import {} from './tablespoon-1'
+import {} from './teaspoon-1'
+
+
+export default {
+
+}
