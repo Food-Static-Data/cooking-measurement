02879e5b8ea40c50fe4e9a0a35c4d5ef4561eebd
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-05 05:54:58 +0300

    Update pinch.js

--- src/pinch.js
+++ src/pinch.js
@@ -1,8 +1,8 @@
+let pinch = {
+americanStandart: "1/16 teaspoon",
+americanStandartOunces: null,
+volume: null,
+weight: null
+};
 
-
-           let pinch = {
-               americanStandart: "1/16 teaspoon",
-               americanStandartOunces: null,
-               volume: null,
-               weight: null
-           };
+export default pinch;
