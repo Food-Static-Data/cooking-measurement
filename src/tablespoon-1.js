04719c704a59a682d26621562968dd3c919a9bc3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-05 05:56:24 +0300

    Update tablespoon-1.js

--- src/tablespoon-1.js
+++ src/tablespoon-1.js
@@ -1,8 +1,10 @@
 
 
-  let tablespoon1 = {
-        americanStandart: "3 teaspoons",
-        americanStandartOunces: "1/2 ounce",
-        volume: "15 milliliters",
-        weight: "14.3 grams"
-      };
+let tablespoon1 = {
+      americanStandart: "3 teaspoons",
+      americanStandartOunces: "1/2 ounce",
+      volume: "15 milliliters",
+      weight: "14.3 grams"
+ };
+
+export default tablespoon1;
