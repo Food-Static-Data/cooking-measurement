d0e368c3ac6a22f8d070d34b69109bb80cf5a278
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-05 05:52:34 +0300

    Update cup1-4.js

--- src/cup1-4.js
+++ src/cup1-4.js
@@ -1,8 +1,10 @@
+//  1/4 cup
 
+let cup1_4 = {                                
+      americanStandart: "4 tablespoons",
+      americanStandartOunces: "2 ounces",
+      volume: "60 milliliters",
+      weight: "56.7 grams"
+    };
 
-    let cup1_4 = {                                //  1/4 cup
-          americanStandart: "4 tablespoons",
-          americanStandartOunces: "2 ounces",
-          volume: "60 milliliters",
-          weight: "56.7 grams"
-        };
+export default cup1_4;
