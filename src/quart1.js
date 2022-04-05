5a7245af9eeed390ed53e95b7a7122d7a83fb6c8
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-05 05:55:50 +0300

    Update quart1.js

--- src/quart1.js
+++ src/quart1.js
@@ -1,8 +1,10 @@
 
 
-    let quart1 = {
-          americanStandart: "4 cups,  2 pints",
-          americanStandartOunces: "32 ounces",
-          volume: ".95 liter",
-          weight: null
-        };
+let quart1 = {
+      americanStandart: "4 cups,  2 pints",
+      americanStandartOunces: "32 ounces",
+      volume: ".95 liter",
+      weight: null
+};
+
+export default quart1;
